edd760053bf3c72218cdc64137d8a77880d1012d
Erich Richter <erich.richter@gmail.com>
2020-05-29 15:31:30 -0700

    reformat html, css, js

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,19 @@
-$(document).ready(function(){
-   
-    $( "#box1" ).click(function() {
-        card.box1Switch();
-        card.box2Reverse();
-        card.box3Reverse();
-        card.box4Reverse();
-    });
-    
-    $( "#box2" ).click(function() {
-         card.box2Switch();
-         card.box3Reverse();
-         card.box1Reverse();
-         card.box4Reverse();
-    });
-    
-    $( "#box3" ).click(function() {
-        card.box3Switch();
-        card.box1Reverse();
-        card.box2Reverse();
-        card.box4Reverse();
-    });
-
-    $( "#box4" ).click(function() {
-        card.box4Switch();
-        card.box1Reverse();
-        card.box2Reverse();
-        card.box3Reverse();
-    });
-    
-    
- 
-});
-
-    
-    
-
-function Card(box1, box2, box3, box4){
+function Card(box1, box2, box3, box4) {
     this.box1 = box1;
     this.box2 = box2;
     this.box3 = box3;
     this.box4 = box4;
+}
 
-}    
+var card = new Card();
+
+Card.prototype.box1Switch = function () {
     
-var card = new Card(); 
-  
-Card.prototype.box1Switch = function(){
-
     box1 = anime({
         targets: '#box1',
         translateX: 83,
         translateY: 400,
-        scale: 7 ,
+        scale: 7,
         backgroundColor: '#zzz',
         duration: 2000,
         loop: false,
@@ -59,13 +21,14 @@ Card.prototype.box1Switch = function(){
         
     });
 }
-Card.prototype.box2Switch = function(){
+
+Card.prototype.box2Switch = function () {
     
     box2 = anime({
         targets: '#box2',
         translateX: -27,
         translateY: 400,
-        scale: 7 ,
+        scale: 7,
         backgroundColor: '#zzz',
         duration: 2000,
         loop: false,
@@ -73,40 +36,41 @@ Card.prototype.box2Switch = function(){
     });
 }
 
-    Card.prototype.box3Switch = function(){
+Card.prototype.box3Switch = function () {
     
-        box2 = anime({
-            targets: '#box3',
-            translateX: -137,
-            translateY: 400,
-            scale: 7 ,
-            backgroundColor: '#zzz',
-            duration: 2000,
-            loop: false,
-            direction: 'normal',
-        });
-    }
+    box2 = anime({
+        targets: '#box3',
+        translateX: -137,
+        translateY: 400,
+        scale: 7,
+        backgroundColor: '#zzz',
+        duration: 2000,
+        loop: false,
+        direction: 'normal',
+    });
+}
+
+Card.prototype.box4Switch = function () {
     
-    Card.prototype.box4Switch = function(){
-    
-        box2 = anime({
-            targets: '#box4',
-            translateX: -247,
-            translateY: 400,
-            scale: 7 ,
-            backgroundColor: '#zzz',
-            duration: 2000,
-            loop: false,
-            direction: 'normal',
-        });
-    } 
-Card.prototype.box1Reverse = function(){
+    box2 = anime({
+        targets: '#box4',
+        translateX: -247,
+        translateY: 400,
+        scale: 7,
+        backgroundColor: '#zzz',
+        duration: 2000,
+        loop: false,
+        direction: 'normal',
+    });
+}
+
+Card.prototype.box1Reverse = function () {
     
     anime({
         targets: '#box1',
         translateX: 0,
         translateY: 0,
-        scale: 1 ,
+        scale: 1,
         backgroundColor: '#zzz',
         duration: 2000,
         loop: false,
@@ -115,13 +79,13 @@ Card.prototype.box1Reverse = function(){
     });
 }
 
-Card.prototype.box2Reverse = function(){
+Card.prototype.box2Reverse = function () {
     
     anime({
         targets: '#box2',
         translateX: 0,
         translateY: 0,
-        scale: 1 ,
+        scale: 1,
         backgroundColor: '#zzz',
         duration: 2000,
         loop: false,
@@ -130,27 +94,28 @@ Card.prototype.box2Reverse = function(){
     });
 }
 
-Card.prototype.box3Reverse = function(){
+Card.prototype.box3Reverse = function () {
     
     anime({
         targets: '#box3',
         translateX: 0,
         translateY: 0,
-        scale: 1 ,
+        scale: 1,
         backgroundColor: '#zzz',
         duration: 2000,
         loop: false,
         direction: 'normal',
         easing: 'cubicBezier(0.490, 0.060, 0.275, 0.970)'
     });
+}
 
-Card.prototype.box4Reverse = function(){
-
+Card.prototype.box4Reverse = function () {
+    
     anime({
         targets: '#box4',
         translateX: 0,
         translateY: 0,
-        scale: 1 ,
+        scale: 1,
         backgroundColor: '#zzz',
         duration: 2000,
         loop: false,
@@ -158,4 +123,34 @@ Card.prototype.box4Reverse = function(){
         easing: 'cubicBezier(0.490, 0.060, 0.275, 0.970)'
     });
 }
-}
+
+$(document).ready(function () {
+
+    $("#box1").click(function () {
+        card.box1Switch();
+        card.box2Reverse();
+        card.box3Reverse();
+        card.box4Reverse();
+    });
+
+    $("#box2").click(function () {
+        card.box2Switch();
+        card.box3Reverse();
+        card.box1Reverse();
+        card.box4Reverse();
+    });
+
+    $("#box3").click(function () {
+        card.box3Switch();
+        card.box1Reverse();
+        card.box2Reverse();
+        card.box4Reverse();
+    });
+
+    $("#box4").click(function () {
+        card.box4Switch();
+        card.box1Reverse();
+        card.box2Reverse();
+        card.box3Reverse();
+    });
+});
